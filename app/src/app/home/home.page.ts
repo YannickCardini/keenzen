ed42e4686794cdb2e2c7cf3a7dafbedd365f43a2
YannickCardini <yannick.cardini@gmx.fr>
2026-02-14 10:37:44 +0700

    Add board :pawn:

--- app/src/app/home/home.page.ts
+++ app/src/app/home/home.page.ts
@@ -11,6 +11,9 @@ export class HomePage implements OnInit {
   gridSize = 11;
   squareSize: number = 0;
 
+  squareToDisplay = [4, 5, 6, 7, 8, 15, 19, 30, 41, 42, 43, 44, 26, 37,
+    34, 35, 36, 45, 55, 56, 66, 67, 86, 87, 88, 77, 79, 78, 80, 81, 85, 92, 96, 103, 107, 114, 115, 116, 117, 118]
+
   constructor() { }
 
   ngOnInit() {
@@ -46,6 +49,11 @@ export class HomePage implements OnInit {
 
   getSquareIndex(row: number, col: number): number {
     return row * this.gridSize + col + 1;
+  }
+
+  shouldDisplayThisSquare(row: number, col: number) {
+    const squareIndex = this.getSquareIndex(row, col);
+    return this.squareToDisplay.includes(squareIndex);
   }
 }
 
