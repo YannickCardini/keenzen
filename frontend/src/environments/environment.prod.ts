37986dd59ac90f1f50b867092fb446f45949beb0
YannickCardini <yannick.cardini@gmx.fr>
2026-02-21 21:00:31 +0700

    Add environment :gear:

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://keenzen-api.azurewebsites.net',
-    wsUrl: 'wss://keenzen-api.azurewebsites.net'
+    apiUrl: 'https://keenzen-api-dae7g0h0hghfhzff.canadacentral-01.azurewebsites.net',
+    wsUrl: 'wss://keenzen-api-dae7g0h0hghfhzff.canadacentral-01.azurewebsites.net'
 };
